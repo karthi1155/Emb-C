8281d627132d8599b829dd308bc78aaa64ceb547
karthi1155 <srikarthi2017@gmail.com>
2021-07-28 09:31:54 +0530

    main

---
+++ main.c
@@ -0,0 +1,32 @@
+#include "all_header.h"
+
+
+int main(void)
+{
+    initPort();
+    initADC();
+    initPWM();
+    //initUSART(103);
+
+    while(1)
+    {
+        int systemStatus;
+        systemStatus = check();
+        
+        if(systemStatus)
+        {
+            char temperature;
+            uint16_t temp;
+
+            temp = readADC(5);
+            _delay_ms(200);
+            temperature = Generate_PWM(temp);
+            _delay_ms(200);
+            //transmitCharUSART(temperature);
+        }
+        else
+        {
+            OCR1A = 0;
+        }
+    }
+}
